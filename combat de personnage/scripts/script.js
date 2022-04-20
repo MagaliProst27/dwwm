79d27f4d46460db35b448e47c3832b2d3aebe274
27010-34-08 <27010-34-08@exchange.ad.afpanet>
2022-04-20 16:32:38 +0200

    combat

--- combat de personnage/scripts/script.js
+++ combat de personnage/scripts/script.js
@@ -1,95 +1,187 @@
+// ------------------------VARIABLES
 let min;
 let max;
+let randomAtq;
+let randomDef;
+let randomAncienAtq = -1;
 min = 20;
 max = 100;
-class Heros {
-    constructor(_nom) {
 
-        this.nombreAleatoire = function () {
-
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-
-        // while (_nom == "") {
-        //     alert("Pas de nom");
-        //     _nom = prompt("Saisis moi un nom");
-
-        // }
-
-        this.nom = _nom;
-        this.vie = this.nombreAleatoire();
-        this.attaque = this.nombreAleatoire();
-        this.defense = this.nombreAleatoire();
-        this.existe = false;
-        if (this.nom != "") {
-            this.existe = true;
-        }
-
-        this.afficherInfo = function () {
-            console.log(`Nom : ${this.nom}, Vie : ${this.vie}, Attaque : ${this.attaque}, Défense : ${this.defense}`);
-
-        }
-        this.attaquer = function (defenseur) {
-
-            console.log(`nouvelle attaque de: ${this.nom} sur ${defenseur.nom}`)
-            if (this.attaque > defenseur.defense) {
-                defenseur.vie = defenseur.vie - 10;
-                console.log(`niveau de vie de ${defenseur.nom}: ${defenseur.vie}`)
-            }
-            if (this.attaque == defenseur.defense) {
-                defenseur.vie = defenseur.vie - 5;
-                console.log(`niveau de vie de ${defenseur.nom}: ${defenseur.vie}`)
-            }
-            if (this.attaque < defenseur.defense) {
-                this.vie = this.vie - 5
-                console.log(`niveau de vie de ${this.nom}: ${this.vie}`)
-            }
-            if (this.vie <= 0) {
-                console.error(`le personnage ${this.nom} est dead.`)
-                this.existe = false
-            }
-            if (defenseur.vie <= 0) {
-                console.error(`le personnage ${defenseur.nom} est dead.`)
-                defenseur.existe = false
-            }
-
-        }
-
+//-------------------------COLOR
+function colorLog(message, color) {
+    switch (color) {
+        case "dead":
+            color = "red";
+            break;
+        case "win":
+            color = "green";
+            break;
+        case "battle":
+            color = "orange";
+            break;
+        case "perso":
+            color = "blue";
+            break;
+        default:
+            color = "black";
+            break;
     }
+    console.log("%c" + message, "color: " + color);
 }
 
-const nbrJoueur = 5;
-var joueurs= new Array();
-var nbrejoueurcree=0;
-var nomSaisie='';
-var perso;
-while(nbrejoueurcree<nbrJoueur && nomSaisie==''){
+// --------------------------------------------CLASS PERSO---------------------------------------------------------
+function marchestp() {
+    class Heros {
+        constructor(nom) {
 
-    nomSaisie=prompt("Saisissez un nom:");
-    if (nomSaisie!= ''){
-        perso= new Heros(nomSaisie);
-        perso.afficherInfo();
-        joueurs.push(perso);
-        nomSaisie = '';
-        nbrejoueurcree+=1;
+            this.nombreAleatoire = function () {
+
+                return Math.floor(Math.random() * (max - min + 1) + min);
+            }
+
+
+            // -----------------------------------------EXISTE
+            var _existe;
+            this.Getexiste = function () {
+                return _existe;
+            }
+            this.Setexiste = function (newexiste) {
+                _existe = newexiste;
+            }
+            // ---------------------------------------NOM
+            var _nom = "";
+            this.Getnom = function () {
+                return _nom;
+            }
+            this.Setnom = function (newnom) {
+                _nom = newnom
+            }
+            if (nom != "") {
+                this.Setnom(nom);
+                _existe = true;
+            }
+            // ------------------------------VIE
+            var _vie = this.nombreAleatoire();
+
+            this.Getvie = function () {
+                return _vie;
+            }
+            this.Setvie = function (newvie) {
+                _vie = newvie;
+                if (this.Getvie() <= 0) {
+                    colorLog("Le personnage " + this.Getnom() + " est dead.", "dead")
+                    this.Setexiste(false);
+                    _vie = 0;
+                }
+            }
+            // ----------------------------------ATTAQUE
+            var _attaque = this.nombreAleatoire();
+
+            this.Getattaque = function () {
+                return _attaque;
+            }
+            this.Setattaque = function (newattaque) {
+                _attaque = newattaque;
+            }
+            // ---------------------------------------DEFENSE
+            var _defense = this.nombreAleatoire();
+            this.Getdefense = function () {
+                return _defense;
+            }
+            this.Setdefense = function (newdefense) {
+                _defense = newdefense
+            }
+            // ----------------------------------------------AFFICHER INFO
+            this.afficherInfo = function () {
+                colorLog(`Nom : ${this.Getnom()}, Vie : ${this.Getvie()}, Attaque : ${this.Getattaque()}, Défense : ${this.Getdefense()}`, "perso");
+            }
+            // ---------------------------------------------- SURPRISE MOTHERFUCKER
+            this.attaquer = function (defenseur) {
+
+                console.log(`nouvelle attaque de: ${this.Getnom()} sur ${defenseur.Getnom()}`)
+                if (this.Getattaque() > defenseur.Getdefense()) {
+                    defenseur.Setvie(defenseur.Getvie() - 10);
+                    colorLog("niveau de vie de " + defenseur.Getnom() + ":" + defenseur.Getvie(), "battle")
+                }
+                if (this.Getattaque() == defenseur.Getdefense()) {
+                    defenseur.Setvie(defenseur.Getvie() - 5);
+                    colorLog("niveau de vie de " + defenseur.Getnom() + ":" + defenseur.Getvie(), "battle")
+                }
+                if (this.Getattaque() < defenseur.Getdefense()) {
+                    this.Setvie(this.Getvie() - 5);
+                    colorLog("Niveau de vie de " + this.Getnom() + " :" + this.Getvie(), "battle")
+                }
+                if (this.Getvie() == 0 && defenseur.Getvie() != 0) {
+                    colorLog("Le joueur " + defenseur.Getnom() + " a gagné", "win")
+                }
+                if (this.Getvie() != 0 && defenseur.Getvie() == 0) {
+                    colorLog("Le joueur " + this.Getnom() + " a gagné", "win")
+                }
+            }
+        }
     }
-
 }
-console.log(joueurs);
+// ----------------------------------------------------------CLASS MATCH----------------------------------------------------
+class Match {
+    marchestp();
+    constructor() {
+        //-------------------------------TABLEAU-----------------------------------------
+        let nbrJoueur = 2;
+        var joueurs = new Array();
+        var nbrejoueurcree = 0;
+        var nomSaisie = '';
+        var perso;
+        while (nbrejoueurcree < nbrJoueur && nomSaisie == '') {
+
+            nomSaisie = prompt("Saisissez un nom:");
+            if (nomSaisie != '') {
+                perso = new Heros(nomSaisie);
+                perso.afficherInfo();
+                joueurs.push(perso);
+                nomSaisie = '';
+                nbrejoueurcree += 1;
+            }
+        }
+
+        // ------FONCTION RANDOM----------
 
 
-// var perso1 = new Heros("jean");
+        function joueurAleatoire(length) {
+            return Math.floor(Math.random() * length);
+        }
 
-// var perso2 = new Heros("bernard");
+        //tant qu'il reste plus d'un joueur
+        while (joueurs.length > 1) {
+            //definit l'attaquant de façon aléatoire
+            randomAtq = joueurAleatoire(joueurs.length);
+            //definit le defenseur 
+            randomDef = joueurAleatoire(joueurs.length);
+            //verifie si l'attaquant est different de l'ancien attaquant
+            if (randomAtq != randomAncienAtq) {
+                //verifie si l'attaquand est different du defenseur 
+                if (randomAtq != randomDef) {
+                    //attaque
+                    joueurs[randomAtq].attaquer(joueurs[randomDef])
+                    try {
+                        //si l'attaquant est mort on le supprime du tableau
+                        if (joueurs[randomAtq].Getexiste() == false) {
+                            joueurs.splice(randomAtq, 1);
+                        }
+                        //si le defenseur est mort on le supprime du tableau
+                        if (joueurs[randomDef].Getexiste() == false) {
+                            joueurs.splice(randomDef, 1);
+                        }
+                        //on intercepte l'erreur si la donnée du tableau n'existe plus 
+                    } catch (error) {
 
-// console.log(perso1);
-// console.log(perso2);
-// perso1.afficherInfo();
-// perso2.afficherInfo();
-// perso1.attaquer(perso2);
-// perso2.attaquer(perso1);
-// perso1.afficherInfo();
-// perso2.afficherInfo();
+                    }
 
 
-// while (perso1.existe != false || perso2.existe != false) {
+                    //l'attaquant devient l'ancien attaquant 
+                    randomAncienAtq = randomAtq;
+                }
+            }
+            console.log(joueurs);
+        }
+    }
+}
