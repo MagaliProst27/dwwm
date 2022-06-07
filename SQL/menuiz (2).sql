42cd4ee9909f3e128d1b9a4b1ad1a190fd1393a7
27010-34-08 <27010-34-08@exchange.ad.afpanet>
2022-06-07 16:35:15 +0200

    basedonnée

---
+++ SQL/menuiz (2).sql
@@ -0,0 +1,456 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 07 juin 2022 à 16:32
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `menuiz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_address_adr`
+--
+
+CREATE TABLE `t_d_address_adr` (
+  `ADR_ID` int(11) NOT NULL,
+  `ADR_FIRSTNAME` varchar(1024) NOT NULL,
+  `ADR_LASTNAME` varchar(1024) NOT NULL,
+  `ADR_LINE1` varchar(1024) NOT NULL,
+  `ADR_LINE2` varchar(1024) DEFAULT NULL,
+  `ADR_LINE3` varchar(1024) DEFAULT NULL,
+  `ADR_ZIPCODE` varchar(1024) NOT NULL,
+  `ADR_CITY` varchar(1024) NOT NULL,
+  `ADR_COUNTRY` varchar(1024) NOT NULL,
+  `ADR_MAIL` varchar(1024) NOT NULL,
+  `ADR_PHONE` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `t_d_address_adr`
+--
+
+INSERT INTO `t_d_address_adr` (`ADR_ID`, `ADR_FIRSTNAME`, `ADR_LASTNAME`, `ADR_LINE1`, `ADR_LINE2`, `ADR_LINE3`, `ADR_ZIPCODE`, `ADR_CITY`, `ADR_COUNTRY`, `ADR_MAIL`, `ADR_PHONE`) VALUES
+(1, 'non renseigné', 'non renseigné', 'non renseigné', NULL, NULL, 'non renseigné', 'non renseigné', '', 'non renseigné', 'non renseigné'),
+(3, 'Louis', 'Emile', '21 rue de lhopital', NULL, NULL, '89000', 'Auxerre', '', 'louis@emile.fr', '0352478796'),
+(4, 'Dutroux', 'Marc', '12 rue de lecole', NULL, NULL, '01235', 'Lille', '', 'dutroux@marc.fr', '0045685452'),
+(5, 'Fourniret', 'Michel', '12 rue du chateau', NULL, NULL, '32456', 'nancy', '', 'fourniret@michel.fr', '0365874964');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_expeditiontype_ety`
+--
+
+CREATE TABLE `t_d_expeditiontype_ety` (
+  `ETY_ID` int(11) NOT NULL,
+  `ETY_WORDING` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `t_d_expeditiontype_ety`
+--
+
+INSERT INTO `t_d_expeditiontype_ety` (`ETY_ID`, `ETY_WORDING`) VALUES
+(1, 'collissimo'),
+(2, 'transporteur interne'),
+(3, 'chronopost');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_expedition_exp`
+--
+
+CREATE TABLE `t_d_expedition_exp` (
+  `EXP_ID` int(11) NOT NULL,
+  `EXP_WEIGTH` decimal(8,2) DEFAULT NULL,
+  `EXP_TRACKINGNUMBER` varchar(1024) DEFAULT NULL,
+  `EXP_SENTDATE` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_orderdetails_odt`
+--
+
+CREATE TABLE `t_d_orderdetails_odt` (
+  `OHR_ID` int(11) NOT NULL,
+  `PRD_ID` int(11) NOT NULL,
+  `EXP_ID` int(11) NOT NULL,
+  `ODT_QUANTITY` int(11) NOT NULL,
+  `ODT_ISCANCELED` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_orderheader_ohr`
+--
+
+CREATE TABLE `t_d_orderheader_ohr` (
+  `OHR_ID` int(11) NOT NULL,
+  `ADR_ID_LIV` int(11) NOT NULL,
+  `ADR_ID_FAC` int(11) NOT NULL,
+  `PMT_ID` int(11) NOT NULL,
+  `OSS_ID` int(11) NOT NULL,
+  `ETY_ID` int(11) NOT NULL,
+  `USR_ID` int(11) NOT NULL,
+  `OHR_NUMBER` varchar(1024) NOT NULL,
+  `OHR_DATE` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_orderstatus_oss`
+--
+
+CREATE TABLE `t_d_orderstatus_oss` (
+  `OSS_ID` int(11) NOT NULL,
+  `OSS_WORDING` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_paymenttype_pmt`
+--
+
+CREATE TABLE `t_d_paymenttype_pmt` (
+  `PMT_ID` int(11) NOT NULL,
+  `PMT_WORDING` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `t_d_paymenttype_pmt`
+--
+
+INSERT INTO `t_d_paymenttype_pmt` (`PMT_ID`, `PMT_WORDING`) VALUES
+(1, 'CB'),
+(2, 'ESPECES'),
+(3, 'VIREMENT'),
+(4, 'CHEQUE');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_productkit_kit`
+--
+
+CREATE TABLE `t_d_productkit_kit` (
+  `PRD_ID_KIT` int(11) NOT NULL,
+  `PRD_ID_COMPONENT` int(11) NOT NULL,
+  `KIT_QUANTITY` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `t_d_productkit_kit`
+--
+
+INSERT INTO `t_d_productkit_kit` (`PRD_ID_KIT`, `PRD_ID_COMPONENT`, `KIT_QUANTITY`) VALUES
+(1, 6, 50),
+(2, 7, 10),
+(3, 8, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_producttype_pty`
+--
+
+CREATE TABLE `t_d_producttype_pty` (
+  `PTY_ID` int(11) NOT NULL,
+  `PTY_DESCRIPTION` varchar(1024) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `t_d_producttype_pty`
+--
+
+INSERT INTO `t_d_producttype_pty` (`PTY_ID`, `PTY_DESCRIPTION`) VALUES
+(1, 'kit'),
+(2, 'unitaire');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_product_prd`
+--
+
+CREATE TABLE `t_d_product_prd` (
+  `PRD_ID` int(11) NOT NULL,
+  `SPL_ID` int(11) NOT NULL,
+  `PTY_ID` int(11) NOT NULL,
+  `PRD_DESCRIPTION` varchar(1024) NOT NULL,
+  `PRD_GUARANTEE` smallint(6) NOT NULL,
+  `PRD_PICTURE` longblob DEFAULT NULL,
+  `PRD_PRICE` decimal(8,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `t_d_product_prd`
+--
+
+INSERT INTO `t_d_product_prd` (`PRD_ID`, `SPL_ID`, `PTY_ID`, `PRD_DESCRIPTION`, `PRD_GUARANTEE`, `PRD_PICTURE`, `PRD_PRICE`) VALUES
+(1, 1, 2, 'produit 1', 24, NULL, '120.44'),
+(2, 2, 2, 'produit 2', 18, NULL, '100.00'),
+(3, 3, 2, 'produit 3', 18, NULL, '99.99'),
+(4, 4, 2, 'produit 4', 36, NULL, '500.00'),
+(5, 5, 2, 'produit 5', 24, NULL, '320.58'),
+(6, 2, 1, 'produit6', 24, NULL, '30.55'),
+(7, 3, 1, 'produit7', 24, NULL, '55.55'),
+(8, 4, 1, 'produit8', 24, NULL, '499.99');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_supplier_spl`
+--
+
+CREATE TABLE `t_d_supplier_spl` (
+  `SPL_ID` int(11) NOT NULL,
+  `SPL_NAME` varchar(1024) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `t_d_supplier_spl`
+--
+
+INSERT INTO `t_d_supplier_spl` (`SPL_ID`, `SPL_NAME`) VALUES
+(1, 'BIOBIERE'),
+(2, 'BIERE CHIC'),
+(3, 'PICON BIERE'),
+(4, 'L\"EURE DE LA BIERE'),
+(5, 'BIBINE');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_d_user_usr`
+--
+
+CREATE TABLE `t_d_user_usr` (
+  `USR_ID` int(11) NOT NULL,
+  `ADR_ID` int(11) DEFAULT NULL,
+  `USR_MAIL` varchar(1024) NOT NULL,
+  `USR_PASSWORD` varchar(1024) NOT NULL,
+  `USR_FIRSTNAME` varchar(1024) NOT NULL,
+  `USR_LASTNAME` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `t_d_address_adr`
+--
+ALTER TABLE `t_d_address_adr`
+  ADD PRIMARY KEY (`ADR_ID`);
+
+--
+-- Index pour la table `t_d_expeditiontype_ety`
+--
+ALTER TABLE `t_d_expeditiontype_ety`
+  ADD PRIMARY KEY (`ETY_ID`);
+
+--
+-- Index pour la table `t_d_expedition_exp`
+--
+ALTER TABLE `t_d_expedition_exp`
+  ADD PRIMARY KEY (`EXP_ID`);
+
+--
+-- Index pour la table `t_d_orderdetails_odt`
+--
+ALTER TABLE `t_d_orderdetails_odt`
+  ADD PRIMARY KEY (`OHR_ID`,`PRD_ID`,`EXP_ID`),
+  ADD KEY `FK_CONCERNE2` (`PRD_ID`),
+  ADD KEY `FK_CONCERNE3` (`EXP_ID`);
+
+--
+-- Index pour la table `t_d_orderheader_ohr`
+--
+ALTER TABLE `t_d_orderheader_ohr`
+  ADD PRIMARY KEY (`OHR_ID`),
+  ADD KEY `FK_A_POUR_PAIEMENT` (`PMT_ID`),
+  ADD KEY `FK_A_POUR_STATUT` (`OSS_ID`),
+  ADD KEY `FK_A_POUR_TYPE_EXPEDITION` (`ETY_ID`),
+  ADD KEY `FK_COMMANDE` (`USR_ID`),
+  ADD KEY `FK_EST_FACTURE` (`ADR_ID_FAC`),
+  ADD KEY `FK_EST_LIVRE` (`ADR_ID_LIV`);
+
+--
+-- Index pour la table `t_d_orderstatus_oss`
+--
+ALTER TABLE `t_d_orderstatus_oss`
+  ADD PRIMARY KEY (`OSS_ID`);
+
+--
+-- Index pour la table `t_d_paymenttype_pmt`
+--
+ALTER TABLE `t_d_paymenttype_pmt`
+  ADD PRIMARY KEY (`PMT_ID`);
+
+--
+-- Index pour la table `t_d_productkit_kit`
+--
+ALTER TABLE `t_d_productkit_kit`
+  ADD PRIMARY KEY (`PRD_ID_KIT`,`PRD_ID_COMPONENT`),
+  ADD KEY `FK_T_D_PROD_SE_COMPOS_T_D_PROD2` (`PRD_ID_COMPONENT`);
+
+--
+-- Index pour la table `t_d_producttype_pty`
+--
+ALTER TABLE `t_d_producttype_pty`
+  ADD PRIMARY KEY (`PTY_ID`);
+
+--
+-- Index pour la table `t_d_product_prd`
+--
+ALTER TABLE `t_d_product_prd`
+  ADD PRIMARY KEY (`PRD_ID`),
+  ADD KEY `FK_EST_DE_TYPE` (`PTY_ID`),
+  ADD KEY `FK_PROVIENT_DE` (`SPL_ID`);
+
+--
+-- Index pour la table `t_d_supplier_spl`
+--
+ALTER TABLE `t_d_supplier_spl`
+  ADD PRIMARY KEY (`SPL_ID`);
+
+--
+-- Index pour la table `t_d_user_usr`
+--
+ALTER TABLE `t_d_user_usr`
+  ADD PRIMARY KEY (`USR_ID`),
+  ADD KEY `FK_T_D_USER_A_COMME_I_T_D_ADDR3` (`ADR_ID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `t_d_address_adr`
+--
+ALTER TABLE `t_d_address_adr`
+  MODIFY `ADR_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_expeditiontype_ety`
+--
+ALTER TABLE `t_d_expeditiontype_ety`
+  MODIFY `ETY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_expedition_exp`
+--
+ALTER TABLE `t_d_expedition_exp`
+  MODIFY `EXP_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_orderheader_ohr`
+--
+ALTER TABLE `t_d_orderheader_ohr`
+  MODIFY `OHR_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_orderstatus_oss`
+--
+ALTER TABLE `t_d_orderstatus_oss`
+  MODIFY `OSS_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_paymenttype_pmt`
+--
+ALTER TABLE `t_d_paymenttype_pmt`
+  MODIFY `PMT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_producttype_pty`
+--
+ALTER TABLE `t_d_producttype_pty`
+  MODIFY `PTY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_product_prd`
+--
+ALTER TABLE `t_d_product_prd`
+  MODIFY `PRD_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_supplier_spl`
+--
+ALTER TABLE `t_d_supplier_spl`
+  MODIFY `SPL_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `t_d_user_usr`
+--
+ALTER TABLE `t_d_user_usr`
+  MODIFY `USR_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `t_d_orderdetails_odt`
+--
+ALTER TABLE `t_d_orderdetails_odt`
+  ADD CONSTRAINT `FK_CONCERNE1` FOREIGN KEY (`OHR_ID`) REFERENCES `t_d_orderheader_ohr` (`OHR_ID`),
+  ADD CONSTRAINT `FK_CONCERNE2` FOREIGN KEY (`PRD_ID`) REFERENCES `t_d_product_prd` (`PRD_ID`),
+  ADD CONSTRAINT `FK_CONCERNE3` FOREIGN KEY (`EXP_ID`) REFERENCES `t_d_expedition_exp` (`EXP_ID`);
+
+--
+-- Contraintes pour la table `t_d_orderheader_ohr`
+--
+ALTER TABLE `t_d_orderheader_ohr`
+  ADD CONSTRAINT `FK_A_POUR_PAIEMENT` FOREIGN KEY (`PMT_ID`) REFERENCES `t_d_paymenttype_pmt` (`PMT_ID`),
+  ADD CONSTRAINT `FK_A_POUR_STATUT` FOREIGN KEY (`OSS_ID`) REFERENCES `t_d_orderstatus_oss` (`OSS_ID`),
+  ADD CONSTRAINT `FK_A_POUR_TYPE_EXPEDITION` FOREIGN KEY (`ETY_ID`) REFERENCES `t_d_expeditiontype_ety` (`ETY_ID`),
+  ADD CONSTRAINT `FK_COMMANDE` FOREIGN KEY (`USR_ID`) REFERENCES `t_d_user_usr` (`USR_ID`),
+  ADD CONSTRAINT `FK_EST_FACTURE` FOREIGN KEY (`ADR_ID_FAC`) REFERENCES `t_d_address_adr` (`ADR_ID`),
+  ADD CONSTRAINT `FK_EST_LIVRE` FOREIGN KEY (`ADR_ID_LIV`) REFERENCES `t_d_address_adr` (`ADR_ID`);
+
+--
+-- Contraintes pour la table `t_d_productkit_kit`
+--
+ALTER TABLE `t_d_productkit_kit`
+  ADD CONSTRAINT `FK_SE_COMPOSE` FOREIGN KEY (`PRD_ID_KIT`) REFERENCES `t_d_product_prd` (`PRD_ID`),
+  ADD CONSTRAINT `FK_T_D_PROD_SE_COMPOS_T_D_PROD2` FOREIGN KEY (`PRD_ID_COMPONENT`) REFERENCES `t_d_product_prd` (`PRD_ID`);
+
+--
+-- Contraintes pour la table `t_d_product_prd`
+--
+ALTER TABLE `t_d_product_prd`
+  ADD CONSTRAINT `FK_EST_DE_TYPE` FOREIGN KEY (`PTY_ID`) REFERENCES `t_d_producttype_pty` (`PTY_ID`),
+  ADD CONSTRAINT `FK_PROVIENT_DE` FOREIGN KEY (`SPL_ID`) REFERENCES `t_d_supplier_spl` (`SPL_ID`);
+
+--
+-- Contraintes pour la table `t_d_user_usr`
+--
+ALTER TABLE `t_d_user_usr`
+  ADD CONSTRAINT `FK_T_D_USER_A_COMME_I_T_D_ADDR3` FOREIGN KEY (`ADR_ID`) REFERENCES `t_d_address_adr` (`ADR_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
