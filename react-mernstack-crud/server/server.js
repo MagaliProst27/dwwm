c4d713abce04cdcef41201d2eff31afaaf87a0c9
27010-34-08 <27010-34-08@exchange.ad.afpanet>
2022-07-06 16:18:51 +0200

    mern

---
+++ react-mernstack-crud/server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+//middlewares
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+//route
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello from Node Server</h1>");
+});
+
+app.listen(27017, () => {
+  console.log("serveur démarré sur port 27017");
+});
